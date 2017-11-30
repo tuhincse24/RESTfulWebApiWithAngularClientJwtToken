51c91bd3362bc5e71372645d03b1226d8cd6ae90
Tuhin <tuhin@tuhin.com>
2017-11-30 21:37:14 +0600

    About Controller added

---
+++ AngularClinetApplication/Scripts/app/security/controllers/aboutController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    var injectParams = [];
+
+    var AboutController = function () {
+
+    };
+
+    AboutController.$inject = injectParams;
+
+    angular.module('angularApp').controller('aboutController', AboutController);
+
+}());
