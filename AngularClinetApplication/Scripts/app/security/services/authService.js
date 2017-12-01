6ea7890491e8cf3f82beeb79db5be5cbecece81a
tuhin <tuhin@test.com>
2017-12-01 21:08:13 +0600

    Remove unnessesary code.

--- AngularClinetApplication/Scripts/app/security/services/authService.js
+++ AngularClinetApplication/Scripts/app/security/services/authService.js
@@ -10,12 +10,6 @@ app.factory('authService', ['$http', '$q', 'localStorageService', 'ngAuthSetting
         useRefreshTokens: false
     };
 
-    var _externalAuthData = {
-        provider: "",
-        userName: "",
-        externalAccessToken: ""
-    };
-
     var _saveRegistration = function (registration) {
 
         _logOut();
@@ -76,91 +70,11 @@ app.factory('authService', ['$http', '$q', 'localStorageService', 'ngAuthSetting
 
     };
 
-    var _refreshToken = function () {
-        var deferred = $q.defer();
-
-        var authData = localStorageService.get('authorizationData');
-
-        if (authData) {
-
-            if (authData.useRefreshTokens) {
-
-                var data = "grant_type=refresh_token&refresh_token=" + authData.refreshToken + "&client_id=" + ngAuthSettings.clientId;
-
-                localStorageService.remove('authorizationData');
-
-                $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
-
-                    localStorageService.set('authorizationData', { token: response.access_token, userName: response.userName, refreshToken: response.refresh_token, useRefreshTokens: true });
-
-                    deferred.resolve(response);
-
-                }).error(function (err, status) {
-                    _logOut();
-                    deferred.reject(err);
-                });
-            }
-        }
-
-        return deferred.promise;
-    };
-
-    var _obtainAccessToken = function (externalData) {
-
-        var deferred = $q.defer();
-
-        $http.get(serviceBase + 'api/account/ObtainLocalAccessToken', { params: { provider: externalData.provider, externalAccessToken: externalData.externalAccessToken } }).success(function (response) {
-
-            localStorageService.set('authorizationData', { token: response.access_token, userName: response.userName, refreshToken: "", useRefreshTokens: false });
-
-            _authentication.isAuthenticated = true;
-            _authentication.userName = response.userName;
-            _authentication.useRefreshTokens = false;
-
-            deferred.resolve(response);
-
-        }).error(function (err, status) {
-            _logOut();
-            deferred.reject(err);
-        });
-
-        return deferred.promise;
-
-    };
-
-    var _registerExternal = function (registerExternalData) {
-
-        var deferred = $q.defer();
-
-        $http.post(serviceBase + 'api/account/registerexternal', registerExternalData).success(function (response) {
-
-            localStorageService.set('authorizationData', { token: response.access_token, userName: response.userName, refreshToken: "", useRefreshTokens: false });
-
-            _authentication.isAuthenticated = true;
-            _authentication.userName = response.userName;
-            _authentication.useRefreshTokens = false;
-
-            deferred.resolve(response);
-
-        }).error(function (err, status) {
-            _logOut();
-            deferred.reject(err);
-        });
-
-        return deferred.promise;
-
-    };
-
     authServiceFactory.saveRegistration = _saveRegistration;
     authServiceFactory.login = _login;
     authServiceFactory.logOut = _logOut;
     authServiceFactory.fillAuthData = _fillAuthData;
     authServiceFactory.authentication = _authentication;
-    authServiceFactory.refreshToken = _refreshToken;
-
-    authServiceFactory.obtainAccessToken = _obtainAccessToken;
-    authServiceFactory.externalAuthData = _externalAuthData;
-    authServiceFactory.registerExternal = _registerExternal;
 
     return authServiceFactory;
 }]);
